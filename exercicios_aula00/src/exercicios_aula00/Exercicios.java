2e1d0dbb1e4791ef0570461b501bb2b0ca8011a7
Isadora Peixoto <133281545+isadoraapg@users.noreply.github.com>
2023-11-16 13:47:54 -0300

    Aula 00 - Back End

---
+++ exercicios_aula00/src/exercicios_aula00/Exercicios.java
@@ -0,0 +1,254 @@
+package exercicios_aula00;
+
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		//1) prog. q leia numero de um funcionario, horas trabalhadas, valor q recebe por hora e calcule o salário do funcionário.
+		//Mostre o número e o salario do funcionario com 2 casas decimais. 
+		
+//		Scanner leitorDoTeclado = new Scanner (System.in);
+//		
+//		String a;
+//		double b;
+//		double c;
+//		double resultado;
+//		
+//		
+//		System.out.println("Digite o número de identificação: ");
+//		a = leitorDoTeclado.next();
+//		
+//		System.out.println("Digite o número de horas trabalhadas: ");
+//		b = leitorDoTeclado.nextDouble();
+//		
+//		System.out.println("Digite o valor pago por hora: ");
+//		c = leitorDoTeclado.nextDouble();
+//		
+//		resultado = b * c;
+//		
+//		System.out.printf("O salário do funcionário %s é de R$ %.2f", a, resultado);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//2) prog q leia o cod de uma peça 1, o numero de peças 1, o valor unitario de cada peça 1, cod peça 2, numero peça 2, valor peça 2. calcular e mostrar valor a ser pago
+		
+//		Scanner sc = new Scanner (System.in);
+//		
+//		String cod1, cod2;
+//		double num1, num2;
+//		double valor1, valor2;
+//		double resultado;
+//		
+//		System.out.println("Digite o cód da peça 1:");
+//		cod1 = sc.next();
+//		
+//		System.out.println("Digite a quantidade de peças 1:");
+//		num1 = sc.nextDouble();
+//		
+//		System.out.println("Qual o valor unitário de cada peça 1:");
+//		valor1 = sc.nextDouble();
+//		
+//		System.out.println("Digite o cód da peça 2:");
+//		cod2 = sc.next();
+//		
+//		System.out.println("Digite a quantidade de peças 2:");
+//		num2 = sc.nextDouble();
+//		
+//		System.out.println("Qual o valor unitário de cada peça 2:");
+//		valor2 = sc.nextDouble();
+//		
+//		resultado = (num1 * valor1) + (num2 * valor2);
+//		
+//		System.out.println("Valor a ser pago: R$" + resultado);
+		
+		
+		
+		
+		
+		
+		
+		
+		//3) prog q leia 3 valores double: A, B e C. 
+		
+//		Scanner sc = new Scanner (System.in);
+//		
+//		double A;
+//		double B;
+//		double C;
+//		double a;
+//		double b;
+//		double c;
+//		double d;
+//		double e;
+//		
+//		
+//		System.out.println("Digite o valor de A: ");
+//		A = sc.nextDouble();
+//		
+//		System.out.println("Digite o valor de B: ");
+//		B = sc.nextDouble();
+//		
+//		System.out.println("Digite o valor de C: ");
+//		C = sc.nextDouble();
+//		
+//		
+//		a = (A*B)/2;
+//		System.out.println("A área do triângulo retangulo de base A e altura C é igual a: " + a);
+//				
+//		b = 3.14159 * (C * C);	
+//		System.out.println("A área do círculo de raio C é igual a: " + b); 
+//		
+//		c = (A + B) * C / 2;
+//		System.out.println("A área do trapézio que tem A e B por bases e C por altura é igual a: " + c);
+//		
+//		d = B * B;
+//		System.out.println("A área do quadrado que tem lado B é igual a: " + d);
+//		
+//		e = A * B;
+//		System.out.println("A área do retângulo que tem lado A e B é igual a: " + e);
+		
+		
+		
+		
+		
+		
+		
+		
+		//4) prog q leia um num inteiro e diz se é negativo ou nao
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int a;
+//		
+//		System.out.println("Digite um número inteiro: ");
+//		a = sc.nextInt();
+//		
+//		if (a >= 0) {
+//			System.out.println("O número é positivo");
+//		}else {
+//			System.out.println("O número é negativo");
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//5) prog q leia num int e diz se é par ou impar
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int a;
+//		
+//		System.out.println("Digite um número inteiro: ");
+//		a = sc.nextInt();
+//		
+//		if (a %2 == 0) {
+//			System.out.println("O número é par");
+//		}else {
+//			System.out.println("O número é ímpar");
+//		}
+		
+		
+		
+		
+		
+		
+		
+		//6) prog q leia um valor qlqr e diga em qual intervalo se encontra ([0,25], [25, 50], [50, 75], [75, 100]).  
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//
+//		double a;
+//
+//		System.out.println("Digite um valor qualquer: ");
+//		a = sc.nextDouble();
+//
+//		if (a >= 0 && a <= 25) {
+//			System.out.println("O valor está no intervalo [0, 25]");
+//		}
+//
+//		else if (a > 25 && a <= 50) {
+//			System.out.println("O valor está no intervalo [25, 50]");
+//		}
+//
+//		else if (a > 50 && a <= 75) {
+//			System.out.println("O valor está no intervalo [50, 75]");
+//		}
+//
+//		else if (a > 75 && a <= 100) {
+//			System.out.println("O valor está no intervalo [75, 100]");
+//
+//		} else {
+//			System.out.println("O valor está fora de intervalo");
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//7) prog q leia 2 valores int (x e y) e mostrar msg se sao multiplos ou nao sao multiplos. incluindo se os valores sao multiplos entre si.
+		
+//		Scanner sc = new Scanner(System.in);
+//
+//		int X, Y;
+//
+//		System.out.println("Digite o valor de X:");
+//		X = sc.nextInt();
+//
+//		System.out.println("Digite o valor de Y:");
+//		Y = sc.nextInt();
+//
+//		if (X > Y) {
+//			if (X % Y == 0) {
+//				System.out.println("São múltiplos.");
+//			} else {
+//				System.out.println("Não são múltiplos.");
+//			}
+//		}
+//
+//		else if (X < Y) {
+//			if (Y % X == 0) {
+//				System.out.println("São múltiplos.");
+//			} else {
+//				System.out.println("Não são múltiplos.");
+//			}
+//		}
+//
+//		else {
+//			System.out.println("São múltiplos.");
+//		}
+
+		
+		
+	
+	
+		
+		
+		
+	}
+
+}
---
+++ exercicios_aula00/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author isadora.galego
+ *
+ */
+module exercicio {
+}
